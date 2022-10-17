22fc476680c8dc7efa59f31f153ebbff777c4dbc
momen-19 <momenkismail@gmail.com>
2022-10-17 12:38:38 +0300

    stores

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:interceptors/app/screens/lunch_screen.dart';
-import 'package:interceptors/app/widgets/test.dart';
 
-import 'app/screens/b_nav/main_screen.dart';
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
